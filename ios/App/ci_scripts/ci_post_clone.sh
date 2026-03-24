86beaf6072f22f3af4b4488300aa97467a9069f7
aaron blum <blumaa@gmail.com>
2026-03-24 12:11:30 +0100

    fix ios build 2

--- ios/App/ci_scripts/ci_post_clone.sh
+++ ios/App/ci_scripts/ci_post_clone.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 # Xcode Cloud post-clone script
-# Installs node_modules so Capacitor SPM local package references resolve
+# Installs Node.js via Homebrew, then installs node_modules
+# so Capacitor SPM local package references resolve
+
+export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
+brew install node
 
 cd "$CI_PRIMARY_REPOSITORY_PATH"
 npm ci
